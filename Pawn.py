b3dfc9749ed07d88c90a14b9be0c2ea46401f4f2
Arad05 <134449729+Arad05@users.noreply.github.com>
2024-11-22 16:45:50 +0200

    First piece

---
+++ Pawn.py
@@ -0,0 +1,15 @@
+from ChessPiece import ChessPiece
+
+
+class Pawn(ChessPiece):
+    def __init__(self,name,white):
+        super().__init__(name, white)
+        counter =0
+    
+    def __str__(self):
+        return super().__str__()
+
+    def possibleMoves(self):
+        #  if 
+        #  if self.countre ==0:
+        return 0
