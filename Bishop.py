e3b491c541f404cce420179c2670c3e935b6f6ad
Arad05 <134449729+Arad05@users.noreply.github.com>
2024-12-05 10:11:11 +0200

    Update Bishop.py

--- Bishop.py
+++ Bishop.py
@@ -15,5 +15,5 @@ class Bishop(ChessPiece):
         ret = []
         for i in range(1,8):
             if str(ChessBoard.board[cordsX+i][cordsY+i]) == str(ChessBoard.null()):
-                ret.append("", cordsX+I, cordsY+i)
+                ret.append(("", cordsX+i, chr(cordsY+i+64)))
         return ret
