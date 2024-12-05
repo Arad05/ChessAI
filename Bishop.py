5d8dfe9756b4564091aa48dbacb9f25af47db2d3
Arad05 <134449729+Arad05@users.noreply.github.com>
2024-12-05 12:07:13 +0200

    Update Bishop.py

--- Bishop.py
+++ Bishop.py
@@ -14,7 +14,8 @@ class Bishop(ChessPiece):
     def getPossibleMoves(self,cordsX,cordsY):
         ret = []
         for i in range(1,8):
-            if str(ChessBoard.board[cordsX+i][cordsY+i]) == str(ChessBoard.null()):
+            if ChessBoard.board[cordsX+i][cordsY+i].white == self.white:
+                if str(ChessBoard.board[cordsX+i][cordsY+i]) == str(ChessBoard.null()):
                 ret.append(("", cordsX+i, chr(cordsY+i+64)))
                 elif str(ChessBoard.board[cordsX+i][cordsY+i]) == str(ChessBoard.null()):
         return ret
